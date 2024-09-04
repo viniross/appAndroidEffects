295121b43067efd0be621da6922a7bd4fd99494c
Vinícius Ross <viniciusrossws@gmail.com>
2024-09-04 10:07:42 -0300

    Back to home page with animation and transparency in img.

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -2,8 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,12 +17,29 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         TextView txtMate = findViewById(R.id.txtMate);
+        Button btnTransp = findViewById(R.id.btnTransp);
         ImageView imgMcqueen = findViewById(R.id.imgMcqueen);
 
         txtMate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 txtMate.animate().translationX(200f).setDuration(500).start();
+            }
+        });
+
+        btnTransp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imgMcqueen.setAlpha(0.5f);
+            }
+        });
+
+        imgMcqueen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent telaMcqueen = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(telaMcqueen);
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
             }
         });
     }
