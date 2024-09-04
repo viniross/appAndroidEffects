330f864c14b527f65f5d7b1d705e7bf0340bf5b2
Vinícius Ross <viniciusrossws@gmail.com>
2024-09-04 09:54:52 -0300

    Transition effect in Mate text done.

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        TextView txtMate = findViewById(R.id.txtMate);
+        ImageView imgMcqueen = findViewById(R.id.imgMcqueen);
+
+        txtMate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtMate.animate().translationX(200f).setDuration(500).start();
+            }
+        });
     }
 }
