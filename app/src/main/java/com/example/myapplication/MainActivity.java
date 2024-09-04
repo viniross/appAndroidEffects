5e11aac4dcf4f10a6d2eba41b4b12b1898504638
Vinícius Ross <viniciusrossws@gmail.com>
2024-09-04 09:25:44 -0300

    Animation Slide Up in the image

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,11 +1,21 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
 
 import android.animation.ObjectAnimator;
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.transition.Fade;
+import android.transition.Transition;
+import android.transition.TransitionManager;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,13 +25,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView txtTitleMcqueen, txtCarros;
+        TextView txtTitleMcqueen = findViewById(R.id.txtTitleMcqueen);
+        TextView txtCarros = findViewById(R.id.txtCarros);
+        Button btnTeste = findViewById(R.id.btnTeste);
+        ImageView imgMate = findViewById(R.id.imgMate);
 
-        txtTitleMcqueen = findViewById(R.id.txtTitleMcqueen);
         txtTitleMcqueen.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
         txtTitleMcqueen.getPaint().setShadowLayer(3, 3, 5, Color.BLACK);
-
-        txtCarros = findViewById(R.id.txtCarros);
 
         txtCarros.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +42,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Slide UP
+        Animation animSlideUp = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_up);
+        btnTeste.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imgMate.startAnimation(animSlideUp);
+            }
+        });
 
     }
 }
